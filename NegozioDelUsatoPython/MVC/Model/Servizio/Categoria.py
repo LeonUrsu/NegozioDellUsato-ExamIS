f171a718a7aca4c9406f7a0ce338c2b725d0bffc
LeonUrsu <leonursu1998@gmail.com>
2022-06-15 19:03:58 +0200

    implementazione metodi Categoria

--- NegozioDelUsatoPython/MVC/Model/Servizio/Categoria.py
+++ NegozioDelUsatoPython/MVC/Model/Servizio/Categoria.py
@@ -5,10 +5,17 @@ class Categoria:
 
     #Costruttore della Categoria, create() in EA
     def __init__(self):
-        codiceCategoria = None
-        impattoCO2 = None
-        nome = None
-        oggettiTotali = None
+        self.codiceCategoria = None
+        self.impattoCO2 = None
+        self.nome = None
+        self.oggettiTotali = None
+
+    #Metodo per aggiungere i valori alla categoria da creare
+    def aggiungiCategoria(self, codiceCategoria, impattoCO2, nome, oggettiTotali)
+        self.codiceCategoria = codiceCategoria
+        self.impattoCO2 = impattoCO2
+        self.nome = nome
+        self.oggettiTotali = oggettiTotali
 
 
     #Distruttore della classe, destroy() in EA
@@ -16,12 +23,16 @@ class Categoria:
         todo
         return
 
-
+    
     # Metodo che permette di clonare un'istanza della classe
     # return Categoria
     def clone(self):
-        todo
-        return
+        categoria = Categoria()
+        categoria.codiceCategoria = self.codiceCategoria
+        categoria.impattoCO2 = self.impattoCO2
+        categoria.nome = self.nome
+        categoria.oggettiTotali = self.oggettiTotali
+        return categoria
 
 
     #Metodo che permette di eliminare una categoria salvata nel database
