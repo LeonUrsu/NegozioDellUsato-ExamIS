bdeb4df9031c91356b99313223602d80b0357ee5
unknown <LeoPeraz>
2022-06-17 17:16:26 +0200

    aggiunte le classi del Model (Attività) con i relativi metodi

---
+++ NegozioDelUsatoPython/MVC/Model/Attività/Account.py
@@ -0,0 +1,18 @@
+
+
+class Account:
+
+    def __init__(self):
+        self.cognome = None
+        self.dataDiNascita = None
+        self.email = None
+        self.IDAccount = None
+        self.IDProdotti = None
+        self.numeroTelefonico = None
+        self.residenza = None
+
+
+    def verificaRichiedente(self):
+        pass
+
+    
---
+++ NegozioDelUsatoPython/MVC/Model/Attività/Amministratore.py
@@ -0,0 +1,34 @@
+
+
+class Amministratore:
+
+
+    def aggiornaAccount(self):
+        pass
+
+    def aggiornaProdotto(self):
+        pass
+
+    def effettuaBackup(self):
+        pass
+
+    def eliminaAccount(self):
+        pass
+
+    def eliminaProdotto(self):
+        pass
+
+    def filtraClienti(self):
+        pass
+
+    def inserisciProdotto(self):
+        pass
+
+    def ricercaAccount(self):
+        pass
+
+    def vendiProdotto(self):
+        pass
+
+    def visualizzaStatistiche(self):
+        pass
