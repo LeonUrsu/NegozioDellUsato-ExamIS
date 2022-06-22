d135f16a95787b9b6b79bdf972877af577aa676d
LeonUrsu <leonursu1998@gmail.com>
2022-06-22 10:54:13 +0200

    fatta classe Logging con i relativi metodi

--- NegozioDelUsatoPython/MVC/Model/SistemService/Logging.py
+++ NegozioDelUsatoPython/MVC/Model/SistemService/Logging.py
@@ -2,41 +2,86 @@ from datetime import datetime
 
 class Logging():
 
-    def __init__(self):
-        pass
-
-
-    # Metodo che prende
-    def aggiungiLogging(self, email, password):
+    #Costruttore della classe
+    def __init__(self, email, password):
         self.email = email
         self.password = password
+        self.tentativi = 0
+        self.prossimoTentativo = datetime.today()
 
 
-    # Metodo che salva le credeniali di Logging su un file
+    # Metodo che salva le credeniali di un utente su un file quando viene inserito nel sistema
+    # newLog = parametro di tipo Logging
+    # return = True if l'operazinone è andata bene
     def salvaLogging(self, newLog):
         fileName = 'Database\Logging\Logging.txt'
         listLogging = File.deserializza(fileName)
+        if self.checkEmailUtente(listLogging, newLog.email):
+            return False
         listLogging.append(newLog)
+        return True
 
 
     # Metodo che gestisce il login di un utente
     # return valore booleano a seconda se il login è andato a buon fine
-    def loging(self, email, password):
-        date_format = '%d/%m/%Y, %H:%M:%S'
-        #item_date = datetime.strptime('7/16/10', "%m/%d/%y")
-        today = date.today() + timedelta(minutes=3)
-        dateToday = today.strftime(date_format)
+    def loging(self, email , password):
         fileName = 'Database\Logging\Logging.txt'
         listLogging = File.deserializza(fileName)
+        today = datetime.today()
         for x in listLogging:
             if x.email == email:
                 if x.password == password:
-                    if x.prossimoTentativo <= dateToday:
+                   if self.verificaDettagliLogin(x):
                         return True
         return False
 
 
-    def timeout(self):
+    #Metodo che verifica la validità dei dettagli del login
+    # log = credenziali di accesso di tipo Logging
+    # return = True if il login è valido
+    def verificaDettagliLogin(self, log):
+        if self.checkData(log) and self.checkTentativi(log):
+            return True
+        else:
+            return False
 
 
+    # Metodo per controllare se la data di accesso al profilo è valida oppure il profilo
+    # risulta bloccato temporaneamente
+    # log = credenziali di accesso di tipo Logging
+    # return = True se la data è valida per effettuare un nuovo accesso
+    def checkData(self, Log):
+        if log.prossimoTentativo >= datetime.today():
+            return False
+        else:
+            return True
 
+
+    # Metodo per controllare se sono permessi altri tentativi di login
+    # log = credenziali di accesso di tipo Logging
+    # return = True if la soglia non è stata ancora raggiunta
+    def checkTentativi(self, log):
+        if Log.tentativi < 5:
+            return True
+        else:
+            self.timeout()
+            return False
+
+
+    # Metodo per gestire il raggiungimento della soglia massima di tentativi permessi all'utente
+    # log = credenziali di accesso di tipo Logging
+    def timeout(self, log):
+        log.prossimoTentativo = datetime.today() + timedelta(minutes=30)
+        log.tentativi = 0
+
+
+    # Metodo che controlla se sul file esiste un utente con lo stesso indirizzo email
+    # per liminare l'inconsistenza dei dati
+    # listLogging = lista di credenziali utente
+    # email = email da verificare
+    # return = True if esiste già l'email nel sistema
+    def checkEmailUtente(self, listLogin, email):
+        for x in listLogin:
+            if x.email == email:
+                return True
+        return False
--- NegozioDelUsatoPython/main.py
+++ NegozioDelUsatoPython/main.py
@@ -1,7 +1,7 @@
 from datetime import *
 
 
-date_format = '%d/%m/%Y, %H/%M/%S '
+date_format = '%d/%m/%Y, %H/%M/%S'
 today = datetime.today()
 tomorrow = datetime.today() + timedelta(minutes=1)
 if tomorrow >= today:
