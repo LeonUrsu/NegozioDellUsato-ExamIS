5064ce3e6e13694f6c772f5c1bf4aae595752213
LeonUrsu <leonursu1998@gmail.com>
2022-07-21 12:36:32 +0200

    lavoro iniziale su Filtri

---
+++ NegozioDelUsatoPython/MVC/Model/SistemService/Filtri.py
@@ -0,0 +1,21 @@
+
+
+
+class Filtri:
+
+    def __init__(self):
+        filtrati = None
+
+    def filtra(self, PrezzoMin, prezzoMax):
+        pass
+
+
+    def filtra(self, PrezzoMin, prezzoMax):
+        pass
+
+
+    def filtra(self, PrezzoMin, prezzoMax):
+        pass
+
+    def filtra(self, PrezzoMin, prezzoMax):
+        pass
