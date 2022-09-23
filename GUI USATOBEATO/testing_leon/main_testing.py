bc4c996870a42313489d224bbe5674c99a6246e4
leonursu <leonursu1998@gmail.com>
2022-09-23 18:22:09 +0200

    restore

---
+++ GUI USATOBEATO/testing_leon/main_testing.py
@@ -0,0 +1,45 @@
+import sys
+
+from products_interface import *
+from Custom_Widgets.Widgets import *
+from ui_interface import *
+from gui import *
+from ui_interface_vuota import *
+
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_InterfaceVuota()
+        self.ui.setupUi(self)
+        #self.ux = Ui_ProductsFrame()
+        #self.ux.setupUi(self)
+
+        self.ui.prodottiBtn.clicked.connect(self.ui.frameVuoto)
+        # APPLY JSON STYLESHEET
+        # self = QMainWindow class
+        # self.ui = Ui_MainWindow / user interface class
+        loadJsonStyle(self, self.ui)
+        ########################################################################
+
+        #Mostra finestra
+        self.show()
+
+
+
+# esegui app
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+    #end
+
+
+
+
+
+
